9aff94acffd1b14e6c08101fc8976e685c8696a3
Felipe Leal <felipewanderson2020@gmail.com>
2019-04-01 18:37:03 -0300

    criando o input text para mensagem

--- src/components/chat/ChatMensagem.js
+++ src/components/chat/ChatMensagem.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import {InputGroup, InputGroupAddon, Button,Input} from 'reactstrap'
 
 const ChatMensagem = props => {
     return (
         <div className='chat-mensagem'>
-            digite sua mensagem....
+            <InputGroup>
+                <Input placeholder='Digite sua mensagem' />
+                <InputGroupAddon addonType='append'>
+                    <Button color='success'>enviar</Button>
+                </InputGroupAddon>
+            </InputGroup>
         </div>
     )
 }
