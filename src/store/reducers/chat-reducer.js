fca4828d9213a8a63e0e01bbe950928caf09a85f
Felipe Leal <felipewanderson2020@gmail.com>
2019-04-04 15:14:08 -0300

    criando o reducer para o chat

---
+++ src/store/reducers/chat-reducer.js
@@ -0,0 +1,15 @@
+const INITIAL_STATE = {
+    mensagem: {}
+}
+
+export default function chat(state = INITIAL_STATE, action = {}){
+    if(action.type == 'ENVIA_MENSAGEM'){
+        return {
+            mensagens: [...state.mensagens, action.payload.mensagem]
+        }else{
+            return state;
+        }
+    }
+     
+
+}
