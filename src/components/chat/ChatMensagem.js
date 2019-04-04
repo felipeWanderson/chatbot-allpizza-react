f19505aaa2a72ab339e8ae046b2e460be67ac020
Felipe Leal <felipewanderson2020@gmail.com>
2019-04-04 16:35:38 -0300

    Implementando a função de enivio de texto no componente ChatMesagem

--- src/components/chat/ChatMensagem.js
+++ src/components/chat/ChatMensagem.js
@@ -1,18 +1,33 @@
-import React from 'react'
-import {InputGroup, InputGroupAddon, Button,Input} from 'reactstrap'
+import React, {Component} from 'react'
+import {InputGroup, InputGroupAddon, Button,Input} from 'reactstrap';
+import { connect } from 'react-redux'; 
 
-const ChatMensagem = props => {
-    return (
-        <div className='chat-mensagem'>
-            <hr />
-            <InputGroup>
-                <Input placeholder='Digite sua mensagem' />
-                <InputGroupAddon addonType='append'>
-                    <Button color='success'>enviar</Button>
-                </InputGroupAddon>
-            </InputGroup>
-        </div>
-    )
+class ChatMensagem extends Component {
+    constructor(props) {
+        super(props)
+        this.inputEnviaTexto = this.inputEnviaTexto.bind(this);
+    }
+
+    inputEnviaTexto(e){
+        if(e.keyCode === 13){
+            console.log(e.target.value);
+        }
+        
+    }
+
+    render(){
+        return (
+            <div className='chat-mensagem'>
+                <hr />
+                <InputGroup>
+                    <Input onKeyDown={this.inputEnviaTexto} placeholder='Digite sua mensagem' />
+                    <InputGroupAddon addonType='append'>
+                        <Button color='success'>enviar</Button>
+                    </InputGroupAddon>
+                </InputGroup>
+            </div>
+        );
+    }
 }
 
-export default ChatMensagem
+export default connect(null,null)(ChatMensagem) 
